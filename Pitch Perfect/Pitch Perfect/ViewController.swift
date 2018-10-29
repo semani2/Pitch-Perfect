b6d6503a6932ada15033fbda3ce71f17c8326ba8
Sai Emani <Sai.Emani@docusign.com>
2018-10-29 16:18:09 -0700

    Resources added, button enabled disabled configured

--- Pitch Perfect/Pitch Perfect/ViewController.swift
+++ Pitch Perfect/Pitch Perfect/ViewController.swift
@@ -11,20 +11,31 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var startRecordingButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print("View did load")
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        stopRecordingButton.isEnabled = false
+    }
+    
     @IBAction func startRecording(_ sender: Any) {
         print("Start recording clicked")
         recordingLabel.text = "Recording started.. Click Stop Recording to stop."
+        startRecordingButton.isEnabled = false
+        stopRecordingButton.isEnabled = true
     }
     
     @IBAction func stopRecoding(_ sender: Any) {
         print("Stop recording clicked")
         recordingLabel.text = "Recording stoped.. Click start Recording to start."
+        startRecordingButton.isEnabled = true
+        stopRecordingButton.isEnabled = false
     }
     
 
